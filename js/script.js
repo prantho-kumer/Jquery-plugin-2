12f00d53cf614108ce234f6d12ef3d1e853c071a
Prantho Kumer <pkdas036@gmail.com>
2020-03-17 13:55:49 +0600

    up

--- js/script.js
+++ js/script.js
@@ -3,4 +3,13 @@ $(document).ready(function () {
         delay: 10,
         time: 1000
     });
+    $('.counter2').counterUp({
+        delay: 1,
+        time: 10000
+    });
+    $('.counter3').counterUp({
+        delay: 100,
+        time: 1000000
+    });
+
 });
